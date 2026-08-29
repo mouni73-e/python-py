657e71b677b77f5b1d1e6e164befff0c62059dab
mouni73-e <indukurimounika0@gmail.com>
2026-08-29 13:58:40 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,16 @@
+let a={
+    title:"psdf",
+    descriptors:"full stack",
+    triner:"name"
+
+}
+console.log(a['title']);
+console.log(typeof(a));
+
+
+let k = {name:"name"};
+let y = k;
+
+k.name = "mouni";
+console.log(k);
+console.log(y);
